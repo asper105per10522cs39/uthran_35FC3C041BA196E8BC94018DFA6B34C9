6f55e1c491f0e682b4c63bf4b7bcd08fef498198
uthran <144430947+asper105per10522cs39@users.noreply.github.com>
2023-09-25 07:55:10 -0700

    Add files via upload

---
+++ challenges unit3/unit 3.1.py
@@ -0,0 +1,21 @@
+class student:
+ def__init__(self,name,roll_number,cgpa):
+   self.name=name
+  self.roll_number=roll_number
+  self.cgpa=cgpa
+
+def sort_students(student_list):
+  sorted_students=sorted(student_list,key=lambda student:student.cgpa,reverse=true)
+  return sorted_students
+students=[
+  student("jack","a124",10),
+  student("hendry",'a125',9.6),
+  student('joel','a126',6.3),
+  student('lional','a127',8.6),
+  
+]
+sorted_students=sort_students(students)
+for student in sorted_students:
+   print("Name:{},roll Number:{},CGPA:{}".format(student.name,
+
+                                  student.roll_number,student.cgpa))
